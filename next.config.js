7096eac024c35521bee4d727b1c55a5fc14a7f19
kontrol1453 <kontrol1453@users.noreply.github.com>
2026-06-25 21:49:40 +0300

    Fix: Add turbopack config to disable Turbopack

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'expo-secure-store': false,
+      };
+    }
+    // mobile klasörünü ignore et
+    config.module.rules.push({
+      test: /[\\/]mobile[\\/]/,
+      loader: 'ignore-loader',
+    });
+    return config;
+  },
+  turbopack: {},
+};
+
+module.exports = nextConfig;
